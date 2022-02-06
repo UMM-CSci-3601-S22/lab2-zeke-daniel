89f22bf4b8454321ffd32300dbaae265364638b7
Zeke <krug0102@morris.umn.edu>
2022-02-05 18:58:09 -0600

    Refactor methods to get Todos
    Refactored all methods and comments to match todo attributes.

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -5,49 +5,49 @@ import io.javalin.http.HttpCode;
 import io.javalin.http.NotFoundResponse;
 
 /**
- * Controller that manages requests for info about users.
+ * Controller that manages requests for info about todos.
  */
 public class TodoController {
 
   private TodoDatabase database;
 
   /**
-   * Construct a controller for users.
+   * Construct a controller for todos.
    * <p>
-   * This loads the "database" of user info from a JSON file and stores that
-   * internally so that (subsets of) users can be returned in response to
+   * This loads the "database" of todo info from a JSON file and stores that
+   * internally so that (subsets of) todos can be returned in response to
    * requests.
    *
-   * @param database the `Database` containing user data
+   * @param database the `Database` containing todo data
    */
   public TodoController(TodoDatabase database) {
     this.database = database;
   }
 
   /**
-   * Get the single user specified by the `id` parameter in the request.
+   * Get the single todo specified by the `id` parameter in the request.
    *
    * @param ctx a Javalin HTTP context
    */
-  public void getUser(Context ctx) {
+  public void getTodo(Context ctx) {
     String id = ctx.pathParam("id");
-    Todo user = database.getUser(id);
-    if (user != null) {
-      ctx.json(user);
+    Todo todo = database.getTodo(id);
+    if (todo != null) {
+      ctx.json(todo);
       ctx.status(HttpCode.OK);
     } else {
-      throw new NotFoundResponse("No user with id " + id + " was found.");
+      throw new NotFoundResponse("No todo with id " + id + " was found.");
     }
   }
 
   /**
-   * Get a JSON response with a list of all the users in the "database".
+   * Get a JSON response with a list of all the todos in the "database".
    *
    * @param ctx a Javalin HTTP context
    */
-  public void getUsers(Context ctx) {
-    Todo[] users = database.listUsers(ctx.queryParamMap());
-    ctx.json(users);
+  public void getTodos(Context ctx) {
+    Todo[] todos = database.listTodos(ctx.queryParamMap());
+    ctx.json(todos);
   }
 
 }
